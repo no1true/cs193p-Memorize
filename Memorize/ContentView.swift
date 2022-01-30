1ba192c6c98d2bb2565f00b1151fbcaa70c66bc5
no1true <no1true@naver.com>
2022-01-30 21:52:18 +0900

    # Assignment I: Memorize 1~3 completed.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Memorize!")
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
                     ForEach(emojis[0 ..< emojiCount], id: \.self ){ emoji in
@@ -22,39 +23,10 @@ struct ContentView: View {
             }
             .foregroundColor(.red)
             Spacer()
-            HStack {
-                remove
-                Spacer()
-                add
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
         .padding(.horizontal)
     }
     
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            VStack {
-                Image(systemName: "minus.circle")
-            }
-        }
-    }
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            VStack {
-                Image(systemName: "plus.circle")
-            }
-        }
-    }
 }
 
 struct CardView: View {
